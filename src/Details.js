96ccbcfa223af16ed8b392332ca5d5a8435cb466
KPq66dw8L <kezhkia@protonmail.com>
2021-05-31 00:00:03 +0200

    added style to details page

--- src/Details.js
+++ src/Details.js
@@ -2,10 +2,61 @@ import { useParams, useLocation } from 'react-router-dom';
 
 const Details = () => {
     const { country } = useLocation();
-    console.log(country);
-    return <div>
-        <h1>hello</h1>
+    const { 
+        flag, 
+        name, 
+        nativeName, 
+        population, 
+        region, 
+        subregion, 
+        capital, 
+        topLevelDomain, 
+        currencies, 
+        languages, 
+        borders } = country; //arrays: currencies, languages, borders, topLevelDomain
+
+    return <div className='details'>
+        <a href='#'>Back</a>
+        <img src={flag} alt={name}></img>
+        
+            
+            <div className='mainLeft'>
+            <h1>{name}</h1>
+                <span>Native Name:ㅤ<p>{nativeName}</p></span>
+                <span>Population:ㅤ<p>{population.toLocaleString()}</p></span>
+                <span>Region:ㅤ<p>{region}</p></span>
+                <span>Sub Region:ㅤ<p>{subregion}</p></span>
+                <span>Capital:ㅤ<p>{capital}</p></span>
+            </div>
+
+            <div className='mainRight'>
+                <span>Top Level Domain:ㅤ<p>{topLevelDomain}</p></span>
+                <span>Currencies: 
+                {currencies.map((currencie, index) =>{
+                    console.log(currencie.name);
+                    return (<p key={index}>ㅤ{currencie.name}</p>);
+                })}
+                </span>
+                
+                <span>Languages: 
+                {languages.map((language, index) =>{
+                    console.log(language.name);
+                    return (<p key={index}>ㅤ{language.name}</p>);
+                })}
+                </span>
+            </div>
+    
+        <div className='borders'>
+            <span>Border Countries: 
+            {borders.map((border, index) =>{
+                console.log(border.name);
+                return (<p key={index}>ㅤ{border}</p>);
+            })}
+            </span>
+        </div>
+        
     </div>;
 }
+
 
 export default Details;
