b448ff80fa3b1909b0a720c48d5f040b1e10edf8
KPq66dw8L <kezhkia@protonmail.com>
2021-05-31 23:27:13 +0200

    ONE CAN ACCESS EVERY COUNTRY FROM URL

--- src/Details.js
+++ src/Details.js
@@ -59,7 +59,10 @@ const Details = () => {
             
         </div>;
     } else {
-        return <Truc countryName="france"/>
+        let splitUrl = (window.location.href).split('/');
+        let lastPartUrlIndex = splitUrl.length - 1;
+        const pays = splitUrl[lastPartUrlIndex];
+        return <Truc countryName={pays}/>
     }
 }
 
@@ -67,7 +70,6 @@ const Truc = ({countryName}) => {
     const url = 'https://restcountries.eu/rest/v2/name/'+countryName;
     const { isLoading, countries, isError } = useFetch(url);
     if(typeof countries[0] != "undefined"){
-        console.log(countries[0]);
         const { 
                     flag, 
                     name, 
@@ -118,75 +120,6 @@ const Truc = ({countryName}) => {
         </div>);
     }
     return <div></div>
-    // const [country, setCountry] = useState();
-
-    // const getCountry = useCallback(async () => {
-    //     const response = await fetch(url);
-    //     const country = await response.json();
-    //     setCountry(country);
-    // },[url]);
-
-    // useEffect(() => {
-    //     getCountry();
-    // }, [url, getCountry]);
-
-    // console.log(country[0].name);
-
-    
 }
-
-// const Truc = ({countryName}) => {
-//     const { isLoading, countries, isError } = useFetch('https://restcountries.eu/rest/v2/name/'+countryName);
-//     console.log(countries[0]);
-
-//     const { 
-//         flag, 
-//         name, 
-//         nativeName, 
-//         population, 
-//         region, 
-//         subregion, 
-//         capital, 
-//         topLevelDomain, 
-//         currencies, 
-//         languages, 
-//         borders } = countries[0]; 
-//     return (<>
-        // <Link to={`/`} >Back</Link>
-        //     <img src={flag} alt={name}></img>
-                
-        //         <div className='mainLeft'>
-        //         <h1>{name}</h1>
-        //             <span>Native Name:ㅤ<p>{nativeName}</p></span>
-        //             <span>Population:ㅤ<p>{population.toLocaleString()}</p></span>
-        //             <span>Region:ㅤ<p>{region}</p></span>
-        //             <span>Sub Region:ㅤ<p>{subregion}</p></span>
-        //             <span>Capital:ㅤ<p>{capital}</p></span>
-        //         </div>
-
-        //         <div className='mainRight'>
-        //             <span>Top Level Domain:ㅤ<p>{topLevelDomain}</p></span>
-        //             <span>Currencies: 
-        //             {currencies.map((currencie, index) =>{
-        //                 return (<p key={index}>ㅤ{currencie.name}</p>);
-        //             })}
-        //             </span>
-                    
-        //             <span>Languages: 
-        //             {languages.map((language, index) =>{
-        //                 return (<p key={index}>ㅤ{language.name}</p>);
-        //             })}
-        //             </span>
-        //         </div>
-        
-        //     <div className='borders'>
-        //         <span>Border Countries: 
-        //         {borders.map((border, index) =>{
-        //             return (<p key={index}>ㅤ{border}</p>);
-        //         })}
-        //         </span>
-        //     </div>
-//     </>);
-// }
 
 export default Details;
